b191f5441ec93ac6b8c0d7f51d5ec4c22e054669
MigzGo <108534858+MigzGo@users.noreply.github.com>
2024-07-10 21:37:06 +0800

    Burger ka sakin meron na

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}", 
-            './Html/**/*.{html,js}'],
+            './Html/**/*.{html,js}',
+            "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors:{
@@ -12,7 +13,9 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
 
