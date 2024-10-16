aafddfca83dcc52f29b2d1fb090c6922a486af15
jeqBauyonnn <kaisthekinq@gmail.com>
2024-10-16 12:45:31 +0800

    Updated anchor link in navigation menu and added id to Contact Us section

--- JoeExpress/joeexpress/src/components/Home/Home.js
+++ JoeExpress/joeexpress/src/components/Home/Home.js
@@ -700,7 +700,7 @@ function Home() {
           <li class="link"><a href="#">Home</a></li>
           <li class="link"><a onClick={handleNavigate}>Menu</a></li>
           <li class="link"><a href="#aboutus">About Us</a></li>
-          <li class="link"><a href="#footer">Contact Us</a></li>
+          <li class="link"><a href="#contactUs">Contact Us</a></li>
         </ul>
       </span>
 
@@ -1397,7 +1397,7 @@ function Home() {
 
 
 {/* Contact Us Section on Landing Page */}
-<div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+<div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8" id="contactUs">
   <div className="absolute inset-x-0 top-0 -z-10 h-full w-full overflow-hidden" aria-hidden="true">
     <div className="absolute inset-x-0 -top-32 sm:-top-48 transform-gpu blur-3xl" style={{ zIndex: '-1' }}>
       <div className="relative left-1/2 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-green-300 to-green-500 opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
