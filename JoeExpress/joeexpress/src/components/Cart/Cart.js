60360c8233fe19ab8ba920ff8dc5ee3734da8ae2
MigzGo <108534858+MigzGo@users.noreply.github.com>
2024-10-22 15:40:50 +0800

    terms and cond

--- JoeExpress/joeexpress/src/components/Cart/Cart.js
+++ JoeExpress/joeexpress/src/components/Cart/Cart.js
@@ -29,6 +29,7 @@ function Cart() {
     const [cmsFacebook,setCmsFacebook] = useState('');
     const [cmsInstagram,setCmsInstagram] = useState('');
     const [mapModal, setMapModal] = useState(false);
+    const [TermsModal,setTermsModal] = useState(false); //modal
 
     const handleMapModal = () => {
         setMapModal(!mapModal);
