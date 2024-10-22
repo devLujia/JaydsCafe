1544a19104cf298897c96b5e823a2951e4679a50
jeqBauyonnn <kaisthekinq@gmail.com>
2024-10-22 14:38:00 +0800

    Updated Cart, Home, and UserModal components with various styling and layout changes, added social media icons, and modified button text and functionality.

--- JoeExpress/joeexpress/src/components/Home/Home.js
+++ JoeExpress/joeexpress/src/components/Home/Home.js
@@ -1566,7 +1566,6 @@ useEffect(() => {
             </div>
         </div>
     </div>
-
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
--- JoeExpress/joeexpress/src/components/UserModal/Delete/DeleteModal.js
+++ JoeExpress/joeexpress/src/components/UserModal/Delete/DeleteModal.js
@@ -29,7 +29,7 @@ function DeleteModal({closeModal, id}) {
                     {/* <!-- Modal header --> */}
                     <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                          DELETE ITEM!
+                          Remove Item
                         </h3>
                         <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=> closeModal(false)}>
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -41,17 +41,22 @@ function DeleteModal({closeModal, id}) {
                     {/* <!-- Modal body --> */}
                     <div class="p-4 md:p-5 space-y-4">
                         <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            Are you sure you want to delete your item from the cart?
+                            Are you sure you want to remove this item from your cart?
                         </p>
                     </div>
                     {/* <!-- Modal footer --> */}
-                    <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button type="button" 
-                        class=" font-medium rounded-lg text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100  dark:text-gray-400 dark:border-gray-600 dark:hover:text-white  text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={()=> closeModal(false)}>Cancel</button>
-                        <button type="button"
-                         onClick={handleDelete}
-                         class="py-2.5 px-5 ms-3 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">Delete</button>
-                    </div>
+                    <div class="flex justify-end items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button type="button" 
+                        class="font-medium rounded-lg text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
+                        onClick={() => closeModal(false)}>
+                        Cancel
+                    </button>
+                    <button type="button"
+                        onClick={handleDelete}
+                        class="py-2.5 px-5 ms-3 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        Delete
+                    </button>
+                </div>
                 </div>
             </div>
           </div>
